2b92662b6bd1c2a1edbdbf46dae656d6c75c409c
PukuMitra <pulakitamitragit@gmail.com>
2026-02-19 20:44:51 +0530

    1st commit

---
+++ turtleTriangle.py
@@ -0,0 +1,15 @@
+from turtle import *
+speed(0)
+setposition(-40,-30)
+bgcolor("black")
+color("aqua")
+
+for i in range(160):
+    rt(i)
+    circle(170,i)
+    fd(100)
+    rt(240)
+    fd(i)
+    lt(1)
+    hideturtle()
+done()
